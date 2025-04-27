490c3111c48b5c1bc3c6b7296c4717842d6adf51
Anderson Araujo <anderson.araujoprog@gmail.com>
2025-04-27 16:47:34 -0300

    ♻️ refactor(`breadcrumb-generator-service.ts`): Update type from `BreadcrumbItem` to `BreadcrumbItemProps` for consistency.

--- src/ui/services/breadcrumb-generator-service/breadcrumb-generator-service.ts
+++ src/ui/services/breadcrumb-generator-service/breadcrumb-generator-service.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from 'inversify'
 
-import { BreadcrumbItem } from '@/ui/components'
+import { BreadcrumbItemProps } from '@/ui/components'
 import { TYPES } from '@/ui/inversify'
 
 import { type IRouteMatcherService } from '../route-matcher-service'
@@ -10,14 +10,14 @@ import { type IBreadcrumbGeneratorService } from './interfaces'
 export class BreadcrumbGeneratorService implements IBreadcrumbGeneratorService {
   constructor(@inject(TYPES.RouteMatcherService) private readonly routeMatcherService: IRouteMatcherService) {}
 
-  generateBreadcrumbs(currentPath: string, translations: Record<string, string>): BreadcrumbItem[] | null {
+  generateBreadcrumbs(currentPath: string, translations: Record<string, string>): BreadcrumbItemProps[] | null {
     const pathSegments = currentPath.split('/').filter(Boolean)
     if (pathSegments.length === 0) return null
 
-    const nonDynamicSegments = pathSegments.filter(segment => !this.routeMatcherService.isDynamicSegment(segment))
+    const nonDynamicSegments = pathSegments.filter((segment) => !this.routeMatcherService.isDynamicSegment(segment))
     if (nonDynamicSegments.length === 0) return null
 
-    const breadcrumbs: BreadcrumbItem[] = []
+    const breadcrumbs: BreadcrumbItemProps[] = []
     let currentPathBuilder = ''
 
     for (let i = 0; i < pathSegments.length; i++) {
