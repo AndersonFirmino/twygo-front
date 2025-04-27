41e13825fce52485adeffc5cae2687d750d7d9d4
Anderson Araujo <anderson.araujoprog@gmail.com>
2025-04-27 00:03:08 -0300

    🛠️ chore(`axios-http-client`): Configure Axios instance with dynamic `VITE_API_URL` environment base URL

--- src/infra/http/axios-http-client.ts
+++ src/infra/http/axios-http-client.ts
@@ -1,12 +1,26 @@
-import axios from 'axios'
+import axios, { type AxiosInstance } from 'axios'
 import { injectable } from 'inversify'
 
 import { type HttpClient } from '@/domain'
 
 @injectable()
 export class AxiosHttpClient implements HttpClient {
+  private readonly axiosInstance: AxiosInstance
+
+  constructor() {
+    const baseUrl = import.meta.env.VITE_API_URL
+
+    if (!baseUrl) {
+      throw new Error('Missing environment variable: VITE_API_URL')
+    }
+
+    this.axiosInstance = axios.create({
+      baseURL: baseUrl,
+    })
+  }
+
   async get<TResponse>(url: string, signal?: AbortSignal): Promise<TResponse> {
-    const response = await axios.get<TResponse>(url, { signal })
+    const response = await this.axiosInstance.get<TResponse>(url, { signal })
     return response.data
   }
 }
